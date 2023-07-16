17c18f42281884bf35e3016838d723d38e53ee7e
werton <mr.werton@gmail.com>
2023-07-16 01:26:28 -0300

    feat: save email and password in local storage with shared preferences

--- app/src/main/java/com/notrew/one/MainActivity.kt
+++ app/src/main/java/com/notrew/one/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.notrew.one
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 
@@ -10,14 +14,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var loginButton: Button
     private lateinit var emailInput: TextView
     private lateinit var passwordInput: TextView
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        preferences = getSharedPreferences("db_local", Context.MODE_PRIVATE)
+
         loginButton = findViewById(R.id.loginButton)
         emailInput = findViewById(R.id.inputEmail)
         passwordInput = findViewById(R.id.inputPassword)
+        getCredentialsLocally()
+
 
         loginButton.setOnClickListener {
             login()
@@ -29,14 +38,29 @@ class MainActivity : AppCompatActivity() {
         val passwordValue = passwordInput.text.toString().trim()
 
         if (emailValue.isNotBlank()) {
+            preferences.edit().putString("email", emailValue).putString("password", passwordValue).apply()
             println(emailValue)
             println(passwordValue)
+            makeText(
+                this,
+                "Login sucesss \uD83D\uDE80.",
+                Toast.LENGTH_SHORT
+            ).show()
         } else {
             Snackbar.make(
                 findViewById(android.R.id.content),
                 "Please the credentials should be informed.",
                 Snackbar.LENGTH_SHORT
             ).show()
+        }
+    }
+
+    private fun getCredentialsLocally() {
+        val email = preferences.getString("email", null)
+        val password = preferences.getString("password", null)
+        if (!email.isNullOrBlank() && !password.isNullOrBlank()) {
+            emailInput.text = email
+            passwordInput.text = password
         }
     }
 }
